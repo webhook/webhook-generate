6b5fb060b9d441d8d867652bae595c4f84063d9e
Ian Kelly <ltsquigs@gmail.com>
2014-01-14 23:48:17 -0800

    seperate into own file (not Grunt file)

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,10 +10,6 @@ module.exports = function(grunt) {
       }
     },
 
-    clean: {
-      build: ["build"]
-    },
-
     connect: {
       server: {
         options: {
@@ -46,218 +42,47 @@ module.exports = function(grunt) {
 
   });
 
-  var Firebase = require('firebase');
-  var request = require('request');
-  var mkdirp = require('mkdirp');
-  var path = require('path');
-  var swig  = require('swig');
+  var generator = require('./generator').generator(grunt.config.get('firebase'), grunt.log);
 
-  swig.setDefaults({ cache: false });
-
-  var fs = require('fs');
-  var glob = require('glob');
-  var tinylr = require('tiny-lr');
-
-  var root = new Firebase('https://' + grunt.config.get('firebase') +  '.firebaseio.com/');
-
-  var getData = function(callback) {
-    root.child("gamesFinder").once('value', function(data) {
-      callback(data.val());
-    }, function(error) {
-      grunt.log.error(error);
-    });
-  }
-
-  var writeTemplate = function(inFile, outFile, params) {
-    var output = swig.renderFile(inFile, params);
-
-    mkdirp.sync(path.dirname(outFile));
-
-    fs.writeFileSync(outFile, output);
-
-    return outFile.replace('./build', '');
-  }
-
-  var renderPages = function (done, cb)  {
-    grunt.log.ok('Rendering Pages\n');
-    getData(function(data) {
-
-      glob('pages/**/*.html', function(err, files) {
-        var fixedFiles = [];
-        files.forEach(function(file) {
-
-          if(path.extname(file) === '.html')
-          {
-            var newFile = file.replace('pages', './build');
-            fixedFiles.push(writeTemplate(file, newFile, { data: data }));
-          }
-        });
-
-        grunt.log.ok('Finished Rendering Pages\n');
-
-        if(cb) cb(fixedFiles, done);
-      });
-
-    });
-  };
-
-  var renderTemplates = function(done, cb) {
-    grunt.log.ok('Rendering Templates');
-
-    getData(function(data) {
-
-      glob('templates/**/*.html', function(err, files) {
-
-        var fixedFiles = [];
-        files.forEach(function(file) {
-          if(path.extname(file) === '.html')
-          {
-            var baseName = path.basename(file, '.html');
-            var newPath = path.dirname(file).replace('templates', './build');
-            var pathParths = path.dirname(file).split(path.sep);
-            var objectName = pathParths[pathParths.length - 1];
-            var items = data[objectName];
-
-            if(baseName === 'list')
-            {
-              // Output should be path + '/index.html'
-              // Should pass in object as 'items'
-              newPath = newPath + '/index.html';
-              fixedFiles.push(writeTemplate(file, newPath, { items: items }));
-
-            } else if (baseName === 'individual') {
-              // Output should be path + id + '/index.html'
-              // Should pass in object as 'item'
-
-              var baseNewPath = newPath;
-              for(var key in items)
-              {
-                var val = items[key];
-                var id = val.id || val.name;
-
-                newPath = baseNewPath + '/' + id + '/index.html';
-                fixedFiles.push(writeTemplate(file, newPath, { item: val }));
-              }
-            }
-          }
-        });
-
-        grunt.log.ok('Finished Rendering Templates');
-
-        if(cb) cb(fixedFiles, done);
-
-      });
-    });
-  }
-
-  var cleanFiles = function(callback) {
-
-      glob('build/**', function(err, files) {
-        var directories = [];
-        var realFiles = [];
-
-        files.forEach(function(file) {
-          if(path.extname(file))
-          {
-            fs.unlinkSync(file);
-          } else if (file) {
-            directories.push(file);
-          }
-        });
-
-        directories.sort().reverse().forEach(function(file) {
-          fs.rmdirSync(file);
-        });
-
-        callback();
-      });
-
-  };
-
-  var buildBoth = function(cb, done) {
-    // clean files
-    cleanFiles(function() {
-      renderTemplates(null, function() {
-        renderPages(done, cb);
-      });
-    });
-
-  };
-
-  var makeScaffolding = function(name) {
-    grunt.log.ok('Creating Scaffolding\n');
-    var directory = 'templates/' + name + '/';
-    mkdirp.sync(directory);
-
-    // TODO make sure name is not plural
-    var individual = directory + 'list.html';
-
-    // TODO use a real pluralize, not just tak s on
-    var list = directory +  'individual.html';
-
-    fs.writeFile(individual, '');
-    fs.writeFile(list, '');
-  };
-
-  var reloadFiles = function(files, done) {
-    request({ url : 'http://localhost:35729/changed?files=' + files.join(','), timeout: 10  }, function(error, response, body) {
-      if(done) done(true);
-    });
-  };
-
-  var watchFirebase = function() {
-    var initial = true;
-    root.child("gamesFinder").on('value', function(data) {
-
-      if(!initial)
-      {
-        buildBoth(reloadFiles);
-      } else {
-        grunt.log.ok('Watching');
-      }
-
-      initial = false;
-    }, function(error) {
-      grunt.log.error(error);
-    });
-  }
-
-  grunt.registerTask('buildTemplates', function() {
+  grunt.registerTask('buildTemplates', 'Generate static files from templates directory', function() {
     var done = this.async();
-    renderTemplates(done, reloadFiles);
+    generator.renderTemplates(done, generator.reloadFiles);
   });
 
-  grunt.registerTask('buildPages', function() {
+  grunt.registerTask('buildPages', 'Generate static files from pages directory', function() {
     var done = this.async();
-    renderPages(done, reloadFiles);
+    generator.renderPages(done, generator.reloadFiles);
   });
 
-  grunt.registerTask('scaffolding', function(name) {
-    makeScaffolding(name);
+  grunt.registerTask('scaffolding', 'Generate scaffolding for a new object', function(name) {
+    generator.makeScaffolding(name);
   });
 
-  grunt.registerTask('watch', function() {
-    tinylr().listen(35729);
+  grunt.registerTask('watch', 'Watch for changes in templates and regenerate site', function() {
+    generator.startLiveReload();
     grunt.task.run('simple-watch');
   })
 
-  grunt.registerTask('watchFirebase', function() {
+  grunt.registerTask('watchFirebase', 'Watch for changes in firebase and regenerate site', function() {
     var done = this.async();
-    watchFirebase();
+    generator.watchFirebase();
+  });
+
+  grunt.registerTask('clean', 'Clean build files', function() {
+    var done = this.async();
+    generator.cleanFiles(null, done);
   });
 
   // Build Task.
-  grunt.registerTask('build', function() {
+  grunt.registerTask('build', 'Clean files and then generate static site into build', function() {
     var done = this.async();
-
-    buildBoth(reloadFiles, done);
+    generator.buildBoth(generator.reloadFiles, done);
   });
 
-  grunt.registerTask('default', ['build', 'connect', 'open', 'concurrent']);
+  grunt.registerTask('default',  'Clean, Build, Start Local Server, and Watch', ['build', 'connect', 'open', 'concurrent']);
 
   grunt.loadNpmTasks('grunt-simple-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-open');
   grunt.loadNpmTasks('grunt-concurrent');
 };
---
+++ generator.js
@@ -0,0 +1,193 @@
+// Requires
+var Firebase = require('firebase');
+var request = require('request');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var swig = require('swig');
+var fs = require('fs');
+var glob = require('glob');
+var tinylr = require('tiny-lr');
+
+// Config requires
+swig.setDefaults({ cache: false });
+
+// Disable console log in various things
+console.log = function () {};
+
+module.exports.generator = function (firebaseUrl, logger) {
+
+  var self = this;
+  logger = logger || { ok: function() {}, error: function() {}, write: function() {} };
+  firebaseUrl = firebaseUrl || '';
+
+  this.root = new Firebase('https://' + firebaseUrl +  '.firebaseio.com/');
+
+  var getData = function(callback) {
+    self.root.child("gamesFinder").once('value', function(data) {
+      callback(data.val());
+    }, function(error) {
+      logger.error(error);
+    });
+  };
+
+  var writeTemplate = function(inFile, outFile, params) {
+    var output = swig.renderFile(inFile, params);
+
+    mkdirp.sync(path.dirname(outFile));
+
+    fs.writeFileSync(outFile, output);
+
+    return outFile.replace('./build', '');
+  };
+
+  this.renderPages = function (done, cb)  {
+    logger.ok('Rendering Pages\n');
+    getData(function(data) {
+
+      glob('pages/**/*.html', function(err, files) {
+        var fixedFiles = [];
+        files.forEach(function(file) {
+
+          if(path.extname(file) === '.html')
+          {
+            var newFile = file.replace('pages', './build');
+            fixedFiles.push(writeTemplate(file, newFile, { data: data }));
+          }
+        });
+
+        logger.ok('Finished Rendering Pages\n');
+
+        if(cb) cb(fixedFiles, done);
+      });
+
+    });
+  };
+
+  this.renderTemplates = function(done, cb) {
+    logger.ok('Rendering Templates');
+
+    getData(function(data) {
+
+      glob('templates/**/*.html', function(err, files) {
+
+        var fixedFiles = [];
+        files.forEach(function(file) {
+          if(path.extname(file) === '.html')
+          {
+            var baseName = path.basename(file, '.html');
+            var newPath = path.dirname(file).replace('templates', './build');
+            var pathParths = path.dirname(file).split(path.sep);
+            var objectName = pathParths[pathParths.length - 1];
+            var items = data[objectName];
+
+            if(baseName === 'list')
+            {
+              // Output should be path + '/index.html'
+              // Should pass in object as 'items'
+              newPath = newPath + '/index.html';
+              fixedFiles.push(writeTemplate(file, newPath, { items: items }));
+
+            } else if (baseName === 'individual') {
+              // Output should be path + id + '/index.html'
+              // Should pass in object as 'item'
+
+              var baseNewPath = newPath;
+              for(var key in items)
+              {
+                var val = items[key];
+                var id = val.id || val.name;
+
+                newPath = baseNewPath + '/' + id + '/index.html';
+                fixedFiles.push(writeTemplate(file, newPath, { item: val }));
+              }
+            }
+          }
+        });
+
+        logger.ok('Finished Rendering Templates');
+
+        if(cb) cb(fixedFiles, done);
+
+      });
+    });
+  };
+
+  this.cleanFiles = function(callback, done) {
+      logger.ok('Cleaning files');
+      glob('build/**', function(err, files) {
+        var directories = [];
+        var realFiles = [];
+
+        files.forEach(function(file) {
+          if(path.extname(file))
+          {
+            fs.unlinkSync(file);
+          } else if (file) {
+            directories.push(file);
+          }
+        });
+
+        directories.sort().reverse().forEach(function(file) {
+          fs.rmdirSync(file);
+        });
+
+        if (callback) callback();
+        if (done) done(true);
+      });
+
+  };
+
+  this.buildBoth = function(cb, done) {
+    // clean files
+    self.cleanFiles(function() {
+      self.renderTemplates(null, function() {
+        self.renderPages(done, cb);
+      });
+    });
+
+  };
+
+  this.makeScaffolding = function(name) {
+    logger.ok('Creating Scaffolding\n');
+    var directory = 'templates/' + name + '/';
+    mkdirp.sync(directory);
+
+    // TODO make sure name is not plural
+    var individual = directory + 'list.html';
+
+    // TODO use a real pluralize, not just tak s on
+    var list = directory +  'individual.html';
+
+    fs.writeFile(individual, '');
+    fs.writeFile(list, '');
+  };
+
+  this.reloadFiles = function(files, done) {
+    request({ url : 'http://localhost:35729/changed?files=' + files.join(','), timeout: 10  }, function(error, response, body) {
+      if(done) done(true);
+    });
+  };
+
+  this.watchFirebase = function() {
+    var initial = true;
+    self.root.child("gamesFinder").on('value', function(data) {
+
+      if(!initial)
+      {
+        self.buildBoth(self.reloadFiles);
+      } else {
+        logger.ok('Watching');
+      }
+
+      initial = false;
+    }, function(error) {
+      logger.error(error);
+    });
+  };
+
+  this.startLiveReload = function() {
+    tinylr().listen(35729);
+  }
+
+  return this;
+};
