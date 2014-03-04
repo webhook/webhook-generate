15970d605a04f927e53ae04c22cb0fd61fbce2c0
Dave Snider <dave.snider@gmail.com>
2014-03-03 16:26:12 -0800

    warning for grunt file editors

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,19 @@
 'use strict';
 module.exports = function(grunt) {
 
-  // Put your own grunt config here
+  // ----------------------------------------------------------
+  // WARNING, BRAVE DEVELOPER
+  // ----------------------------------------------------------
+  // Webhook allows you to use local grunt tasks and files.
+  // However, these tasks are ONLY RUN LOCALLY and not when
+  // your live site needs to be rebuilt. This means you should
+  // only use grunt for pre-processing tasks like building
+  // Sass, less or coffescript files, not for reading things
+  // from your templates and making dynamic changes during
+  // the build process. Doing so will cause your live site
+  // not to regerate.
+  //
+  // You have been warned!
   grunt.initConfig({
 
   });
