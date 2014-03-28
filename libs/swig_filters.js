b4768bdc7ac435f720454a41fcdc729592c375a5
Ian Kelly <ltsquigs@gmail.com>
2014-03-28 10:50:31 -0700

    add starts with and ends with

--- libs/swig_filters.js
+++ libs/swig_filters.js
@@ -4,6 +4,19 @@ var _ = require('lodash');
 var utils = require('./utils.js');
 var marked = require('marked');
 
+if (typeof String.prototype.startsWith != 'function') {
+  // see below for better implementation!
+  String.prototype.startsWith = function (str){
+    return this.indexOf(str) == 0;
+  };
+}
+
+if (typeof String.prototype.endsWith != 'function') {
+  String.prototype.endsWith = function (str){
+    return this.slice(-str.length) == str;
+  };
+}
+
 /**
  * Defines a set of filters available in swig templates
  * @param  {Object}   swig        Swig engine to add filters to
@@ -138,6 +151,22 @@ module.exports.init = function (swig) {
     return marked(input);
   }
 
+  var startsWith = function(input, string) {
+    if(typeof(input) !== "string") {
+      return false;
+    }
+
+    return input.startsWith(string);
+  };
+
+  var endsWith = function(input, string) {
+    if(typeof(input) !== "string") {
+      return false;
+    }
+    
+    return input.endsWith(string);
+  };
+
   this.setSiteDns = function(dns) {
     siteDns = dns;
   }
@@ -147,6 +176,8 @@ module.exports.init = function (swig) {
   swig.setFilter('upper', upper);
   swig.setFilter('slice', slice);
   swig.setFilter('sort', sort);
+  swig.setFilter('startsWith', startsWith);
+  swig.setFilter('endsWith', endsWith)
   swig.setFilter('reverse', reverse);
   swig.setFilter('imageSize', imageSize);
   swig.setFilter('imageCrop', imageCrop);
