9565a477916eb206a7e427603c6105f216cfe793
Ian Kelly <ltsquigs@gmail.com>
2014-06-14 10:45:15 -0700

    update open warning

--- tasks/generatorTasks.js
+++ tasks/generatorTasks.js
@@ -78,7 +78,9 @@ module.exports = function(grunt) {
       opts: { stdio: 'inherit' }
     }, function (err, result, code) {
       if (err || code > 0) {
-        grunt.warn(result.stderr || result.stdout);
+        grunt.log.warn('A problem occured while trying to open a browser window to connect to the site.')
+        grunt.log.warn(result.stderr || result.stdout);
+        grunt.log.warn('In order to access the site, please navigate to \'localhost:2002\' in your web browser.')
       }
       grunt.log.writeln('\n' + result.stdout);
     });
