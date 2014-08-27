29b81455e150363054a89e904469612e79719668
Ian Kelly <ltsquigs@gmail.com>
2014-08-27 15:19:29 -0700

    generate slug method

--- libs/generator.js
+++ libs/generator.js
@@ -949,8 +949,11 @@ module.exports.generator = function (config, logger, fileParser) {
         } else if (message === 'supported_messages') {
           sock.send('done:' + JSON.stringify([
             'scaffolding', 'scaffolding_force', 'check_scaffolding', 'reset_files', 'supported_messages',
-            'push', 'build', 'preset', 'layouts', 'preset_localv2'
+            'push', 'build', 'preset', 'layouts', 'preset_localv2', 'generate_slug'
           ]));
+        } else if (message === 'generate_slug') {
+          var name = JSON.decode(message.replace('generate_slug:', ''));
+          sock.send('done:' + JSON.stringify(slug(name).toLowerCase()));
         } else if (message === 'push') {
           pushSite(function(error) {
             if(error) {
