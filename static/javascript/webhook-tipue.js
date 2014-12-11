e111a7bda3489c65505be4c25a7295913717719d
Ian Kelly <ltsquigs@gmail.com>
2014-12-11 12:28:43 -0800

    prep for uploading

--- static/javascript/webhook-tipue.js
+++ static/javascript/webhook-tipue.js
@@ -7,14 +7,14 @@ Tipue Search is released under the MIT License
 http://www.tipue.com/search
 */ 
 
-var tipuesearch_stop_words = ["and", "be", "by", "do", "for", "he", "how", "if", "is", "it", "my", "not", "of", "or", "the", "to", "up", "what", "when"];
+var tipuesearch_stop_words = tipuesearch_stop_words || ["and", "be", "by", "do", "for", "he", "how", "if", "is", "it", "my", "not", "of", "or", "the", "to", "up", "what", "when"];
 
-var tipuesearch_replace = {"words": [
+var tipuesearch_replace = tipuesearch_replace || {"words": [
      {"word": "tipua", "replace_with": "tipue"},
      {"word": "javscript", "replace_with": "javascript"}
 ]};
 
-var tipuesearch_stem = {"words": [
+var tipuesearch_stem = tipuesearch_stem || {"words": [
      {"word": "e-mail", "stem": "email"},
      {"word": "javascript", "stem": "script"},
      {"word": "javascript", "stem": "js"}
